d67f11b3dbe5d2337dff1660ef2c49d0400e6215
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-04-29 21:37:58 +0700

    update: remove roleMiddleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,33 +1,6 @@
 import { MiddlewareHandler } from "hono";
-import { getDb } from "@/db";
-import { usersTable } from "@/modules/users/users.model";
-import { eq } from "drizzle-orm";
 import { UnauthorizedError } from "@/errors/http-errors";
 import { verifyToken, JWKS } from "./helper";
-
-export const roleMiddleware: MiddlewareHandler = async (c, next) => {
-  const userFirebase = c.get("userFirebase");
-
-  if (!userFirebase) {
-    throw new UnauthorizedError("Missing userFirebase");
-  }
-
-  const db = getDb(c.env);
-
-  const userRow = await db
-    .select()
-    .from(usersTable)
-    .where(eq(usersTable.id, userFirebase.uid))
-    .get();
-
-  if (!userRow) throw new UnauthorizedError("User not registered");
-
-  c.set("userRole", {
-    role: userRow.role,
-  });
-
-  await next();
-};
 
 export const firebaseAuthMiddleware: MiddlewareHandler = async (c, next) => {
   const payload = await verifyToken(
@@ -45,6 +18,7 @@ export const firebaseAuthMiddleware: MiddlewareHandler = async (c, next) => {
     email: payload.email,
     name: payload.name,
     photoUrl: payload.picture,
+    role: payload.role,
   });
 
   await next();
