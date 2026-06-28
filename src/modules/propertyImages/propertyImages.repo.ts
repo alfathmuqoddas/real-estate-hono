28a6e8b0e480be46210bcd650201349f010febf8
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-06-28 21:48:29 +0700

    update: convert propertyImages.repo into functional

--- src/modules/propertyImages/propertyImages.repo.ts
+++ src/modules/propertyImages/propertyImages.repo.ts
@@ -1,21 +1,20 @@
 import { CreatePropertyImageInput } from "./dto";
 import { propertyImagesTable } from "./propertyImages.model";
 import { eq } from "drizzle-orm";
+import type { DB } from "@/db";
 
-export class PropertyImageRepository {
-  constructor(private db: ReturnType<typeof import("@/db").getDb>) {}
-
-  async create(input: CreatePropertyImageInput[], userId: string) {
+export const PropertyImageRepository = {
+  async create(input: CreatePropertyImageInput[], userId: string, db: DB) {
     const data = input.map((property) => ({
       ...property,
       userId,
     }));
-    return await this.db.insert(propertyImagesTable).values(data).returning();
-  }
+    return await db.insert(propertyImagesTable).values(data).returning();
+  },
 
-  async findByPropertyId(propertyId: string) {
-    return await this.db.query.propertyImagesTable.findMany({
+  async findByPropertyId(propertyId: string, db: DB) {
+    return await db.query.propertyImagesTable.findMany({
       where: eq(propertyImagesTable.propertyId, propertyId),
     });
-  }
-}
+  },
+};
