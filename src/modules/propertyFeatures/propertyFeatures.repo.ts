3d702745eca2e7137e9264b0cdde26ec119b4ce0
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-06-28 21:49:03 +0700

    update: convert propertyFeatures.repo into functional

--- src/modules/propertyFeatures/propertyFeatures.repo.ts
+++ src/modules/propertyFeatures/propertyFeatures.repo.ts
@@ -1,39 +1,35 @@
 import { eq } from "drizzle-orm";
 import { propertyFeaturesTable } from "./propertyFeatures.model";
 import type { PropertyFeatures } from "./schema";
+import type { DB } from "@/db";
 
-export class PropertyFeaturesRepository {
-  constructor(private db: ReturnType<typeof import("@/db").getDb>) {}
+export const PropertyFeaturesRepository = {
+  async findAll(db: DB) {
+    return await db.query.propertyFeaturesTable.findMany();
+  },
 
-  async findAll() {
-    return await this.db.query.propertyFeaturesTable.findMany();
-  }
-
-  async findById(id: string) {
-    return await this.db.query.propertyFeaturesTable.findFirst({
+  async findById(id: string, db: DB) {
+    return await db.query.propertyFeaturesTable.findFirst({
       where: eq(propertyFeaturesTable.id, Number(id)),
     });
-  }
+  },
 
-  async create(input: PropertyFeatures[]) {
-    return await this.db
-      .insert(propertyFeaturesTable)
-      .values(input)
-      .returning();
-  }
+  async create(input: PropertyFeatures[], db: DB) {
+    return await db.insert(propertyFeaturesTable).values(input).returning();
+  },
 
-  async update(id: string, input: Partial<PropertyFeatures>) {
-    return await this.db
+  async update(id: string, input: Partial<PropertyFeatures>, db: DB) {
+    return await db
       .update(propertyFeaturesTable)
       .set(input)
       .where(eq(propertyFeaturesTable.id, Number(id)))
       .returning();
-  }
+  },
 
-  async delete(id: string) {
-    return await this.db
+  async delete(id: string, db: DB) {
+    return await db
       .delete(propertyFeaturesTable)
       .where(eq(propertyFeaturesTable.id, Number(id)))
       .returning();
-  }
-}
+  },
+};
