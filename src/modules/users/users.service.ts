7e42e63a522b5f6bf2339dc2954136a9fb5b2cf5
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-06-28 21:48:04 +0700

    update: convert users.service into functional

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,23 +1,22 @@
 import { CreateUserInput } from "./dto";
-import { UserRepository } from "./users.repo";
+import { UserRepository as repo } from "./users.repo";
 import { BadRequestError, NotFoundError } from "@/errors/http-errors";
+import type { DB } from "@/db";
 
-export class UserService {
-  constructor(private repo: UserRepository) {}
-
-  async getUserById(id: string) {
+export const UserService = {
+  async getUserById(id: string, db: DB) {
     if (!id) {
       throw new BadRequestError("User id is required");
     }
-    const result = await this.repo.findById(id);
+    const result = await repo.findById(id, db);
 
     if (!result) {
       throw new NotFoundError("User not found");
     }
     return result;
-  }
+  },
 
-  async syncUserData(payload: CreateUserInput) {
-    await this.repo.syncUserData(payload);
-  }
-}
+  async syncUserData(payload: CreateUserInput, db: DB) {
+    await repo.syncUserData(payload, db);
+  },
+};
