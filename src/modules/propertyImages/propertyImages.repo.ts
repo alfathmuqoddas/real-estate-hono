42544aafb32df6f090ee504c6af19969e99a0513
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-06-29 11:38:13 +0700

    update: convert propertyImages.repo into functional

--- src/modules/propertyImages/propertyImages.repo.ts
+++ src/modules/propertyImages/propertyImages.repo.ts
@@ -12,6 +12,30 @@ export const PropertyImageRepository = {
     return await db.insert(propertyImagesTable).values(data).returning();
   },
 
+  async delete(id: string, db: DB) {
+    return await db
+      .delete(propertyImagesTable)
+      .where(eq(propertyImagesTable.id, id))
+      .returning();
+  },
+
+  async findAll(db: DB) {
+    return await db.query.propertyImagesTable.findMany();
+  },
+
+  async findById(id: string, db: DB) {
+    return await db.query.propertyImagesTable.findFirst({
+      where: eq(propertyImagesTable.id, id),
+    });
+  },
+
+  async findByUserId(userId: string, db: DB) {
+    const where = eq(propertyImagesTable.userId, userId);
+    return await db.query.propertyImagesTable.findMany({
+      where,
+    });
+  },
+
   async findByPropertyId(propertyId: string, db: DB) {
     return await db.query.propertyImagesTable.findMany({
       where: eq(propertyImagesTable.propertyId, propertyId),
