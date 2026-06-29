5c6186e4b25f626f71e8b2bb40e8f35ba63c74bd
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-06-29 11:38:22 +0700

    update: convert propertyImages.service into functional

--- src/modules/propertyImages/propetyImages.service.ts
+++ src/modules/propertyImages/propetyImages.service.ts
@@ -1,15 +1,20 @@
 import { PropertyImageRepository as repo } from "./propertyImages.repo";
 import type { CreatePropertyImageInput } from "./dto";
-import { BadRequestError } from "@/errors/http-errors";
+import {
+  BadRequestError,
+  ForbiddenError,
+  NotFoundError,
+} from "@/errors/http-errors";
 import type { DB } from "@/db";
+import type { UserContext } from "@/types";
 
 export const PropertyImageService = {
   async createPropertyImage(
     input: CreatePropertyImageInput[],
-    userId: string,
+    user: UserContext["userFirebase"],
     db: DB,
   ) {
-    if (!userId) {
+    if (!user.uid) {
       throw new BadRequestError("User id is required");
     }
     if (input.length === 0) {
@@ -18,9 +23,79 @@ export const PropertyImageService = {
     if (!Array.isArray(input)) {
       throw new BadRequestError("Body must be an array");
     }
-    const result = await repo.create(input, userId, db);
+
+    if (user.role !== "admin" && user.role !== "agent") {
+      throw new ForbiddenError(
+        "Only admins or agent can create property images",
+      );
+    }
+
+    const result = await repo.create(input, user.uid, db);
 
     return { message: `Succesfully created ${result.length} images` };
+  },
+
+  async deletePropertyImage(
+    id: string,
+    user: UserContext["userFirebase"],
+    db: DB,
+  ) {
+    if (!id) {
+      throw new BadRequestError("Property image id is required");
+    }
+
+    const propertyImage = await repo.findById(id, db);
+
+    if (!propertyImage) {
+      throw new NotFoundError("Property image not found");
+    }
+
+    const isAdmin = user.role === "admin";
+    const isAgentOwner =
+      user.role === "agent" && propertyImage.userId === user.uid;
+
+    if (!isAdmin && !isAgentOwner) {
+      throw new ForbiddenError(
+        "You are not authorized to delete this property image",
+      );
+    }
+
+    return await repo.delete(id, db);
+  },
+
+  async getAllPropertyImages(user: UserContext["userFirebase"], db: DB) {
+    if (user.role !== "admin") {
+      throw new ForbiddenError("Only admins can view all property images");
+    }
+
+    return await repo.findAll(db);
+  },
+
+  //for admin
+  async getPropertyImagesByUserId(
+    userId: string,
+    user: UserContext["userFirebase"],
+    db: DB,
+  ) {
+    if (!userId) {
+      throw new BadRequestError("User id is required");
+    }
+
+    if (user.role !== "admin") {
+      throw new ForbiddenError(
+        "Only admins can view property images by user id",
+      );
+    }
+
+    return await repo.findByUserId(user.uid, db);
+  },
+
+  async getMyPropertyImages(user: UserContext["userFirebase"], db: DB) {
+    if (!user.uid) {
+      throw new BadRequestError("User id is required");
+    }
+
+    return await repo.findByUserId(user.uid, db);
   },
 
   async getPropertyImages(propertyId: string, db: DB) {
